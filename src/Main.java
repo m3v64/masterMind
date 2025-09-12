602ab85973748eaad0d6129733b4a639700deb2f
M3v_ <max@m3v.dev>
2025-09-13 00:16:19 +0200

    the first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String colors[] = {"Red", "Green", "Yellow", "Orange", "Purple", "Blue", "space"};
+        String answer[] = new String[4];
+        int i = 0;
+
+        while (i < 4) {
+            int rand = (int) (Math.random() * 7);
+            answer[i] = colors[rand];
+            System.out.println(answer[i]);
+            i++;
+        }
+    }
+}
