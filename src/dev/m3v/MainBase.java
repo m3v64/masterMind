0ee16a164b2f8abc06e0ede11da914b7c2f21908
M3v_ <max@m3v.dev>
2025-09-13 22:45:59 +0200

    updated the ui

--- src/dev/m3v/MainBase.java
+++ src/dev/m3v/MainBase.java
@@ -1,38 +1,84 @@
 package dev.m3v;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainBase {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         int numberOfColors = 4;
         int index = 0;
         int turn = 0;
+        int maxTurns = 10;
         boolean gameOver = false;
 
-        String colors[] = {"Red", "Green", "Yellow", "Orange", "Purple", "Blue", "Space"};
+        clearTerminal();
+        
+        System.out.println("Welcome to Mastermind!");
+        System.out.println("You have 10 turns to guess the correct sequence.");
+        System.out.println("The possible colors are: Red, Green, Yellow, Orange, Purple and Blue");
+        System.out.println("when the game starts choose "+numberOfColors+" colors (separated by a space, for example: Red Green Blue Yellow): ");
+        System.out.println("Each round you get "+numberOfColors+" hints: 'black' means you have a correct color in the correct position, 'white' means you have a correct color in the wrong position and 'none' means that color is not in the sequence.");
+        System.out.println("there are 4 difficulties: easy (4 colors, 12 turns), normal (6 colors, 10 turns), hard (8 colors, 8 turns), very hard (10 colors, 6 turns)");
+        System.out.print("What difficulty do you want to play?: ");
+
+        String difficulty = scanner.nextLine().toLowerCase();
+        switch (difficulty) {
+            case "easy":
+                numberOfColors = 4;
+                maxTurns = 12;
+                break;
+            case "normal":
+                numberOfColors = 6;
+                maxTurns = 10;
+                break;
+            case "hard":
+                numberOfColors = 8;
+                maxTurns = 8;
+                break;
+            case "very hard":
+                numberOfColors = 10;
+                maxTurns = 6;
+                break;
+            default:
+                System.out.println("Invalid difficulty, defaulting to normal.");
+                numberOfColors = 6;
+                maxTurns = 10;
+                break;
+        }
+        System.out.println("Good luck!");
+        System.out.println("Press Enter to start the game...");
+        scanner.nextLine();
+
+        String colors[] = {"Red", "Green", "Yellow", "Orange", "Purple", "Blue"};
         String hints[] = new String[numberOfColors];
+        String answer[] = new String[numberOfColors];
         boolean usedAnswer[] = new boolean[numberOfColors];
         boolean usedGuess[] = new boolean[numberOfColors];
-        String answer[] = new String[numberOfColors];
+        String[] historyGuesses = new String[maxTurns];
+        String[] historyHints = new String[maxTurns];
 
         while (index < numberOfColors) {
-            int rand = (int) (Math.random() * 7);
+            int rand = (int) (Math.random() * 6);
             answer[index] = colors[rand];
             index++;
         }
 
         while (!gameOver) {
-            if (turn == 0) {
-                System.out.println("Welcome to Mastermind!");
-                System.out.println("You have 10 turns to guess the correct sequence.");
-                System.out.println("The colors are: Red, Green, Yellow, Orange, Purple, Blue and Space");
-                System.out.println("when the game starts choose "+numberOfColors+" colors (separated by a space, for example: Red Green Blue Yellow): ");
-                System.out.println("Each round you get "+numberOfColors+" hints: 'black' means you have a correct color in the correct position, 'white' means you have a correct color in the wrong position and 'none' means that color is not in the sequence.");
-                System.out.println("Good luck!");
-            }
+            Arrays.fill(hints, null);
+            Arrays.fill(usedAnswer, false);
+            Arrays.fill(usedGuess, false);
 
             try {
+                clearTerminal();
+                if (turn > 0) {
+                    System.out.println("The possible colors are: Red, Green, Yellow, Orange, Purple and Blue");
+                    System.out.println("Previous moves:");
+                    for (int i = 0; i < turn; i++) {
+                        System.out.println((i + 1) + ". Guess: " + historyGuesses[i] + "  |  Hints: " + historyHints[i]);
+                    }
+                    System.out.println();
+                }
                 System.out.println("choose "+numberOfColors+" colors: ");
                 String input = scanner.nextLine();
                 String[] inputColors = input.split(" ");
@@ -58,6 +104,9 @@ public class MainBase {
                     if (!usedGuess[i]) {
                         hints[i] = "none";
                     }
+
+                    historyGuesses[turn] = String.join(" ", inputColors);
+                    historyHints[turn] = String.join(" ", hints);
                 }
             } catch (Exception e) {
                 System.out.println("Invalid input. Please enter "+numberOfColors+" colors separated by spaces.");
@@ -69,6 +118,7 @@ public class MainBase {
                 gameOver = true;
             } else if (turn == 9) {
                 System.out.println("you have lost! the correct sequence was: " + String.join(" ", answer));
+                gameOver = true;
             } else {
                 System.out.println("your hints are: " + String.join(" ", hints));
             }
@@ -77,6 +127,12 @@ public class MainBase {
         }
 
         scanner.close();
+    }
+
+    public static void clearTerminal() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
     }
 
     private static boolean printHintsAndCheckWin(String[] hints) {
